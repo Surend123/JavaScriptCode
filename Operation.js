67f9b76525557430a8f2cf7777ef6f750a94770d
Surend123 <sky958447@gmail.com>
2024-06-27 04:28:28 +0000

    Operation

---
+++ Operation.js
@@ -0,0 +1,21 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+let str1="surendra",str2=" kumar",str3=" yadav";
+let str4=str1+str2+str3;
+// console.log(str4);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+1+2);
+// console.log(1+2+"1");
+// console.log(3+4*5/3);
+console.log(true);
+console.log(+true);
+//console.log(true+);//Given a Error
+console.log(+" ");
+console.log(+"hi");
+let a=100;
+//console.log(a++);
+console.log(++a);// print after double increment;
