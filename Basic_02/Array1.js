77ba22f1ae5c7e1f966e3793af2d4cf60b27e652
Surend123 <sky958447@gmail.com>
2024-06-30 14:31:14 +0000

    Array in js

--- Basic_02/Array1.js
+++ Basic_02/Array1.js
@@ -1,2 +1,18 @@
 const arr=[0,1,2,3,4,5,];
 console.log(arr);
+arr.push(6);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift(9);
+console.log(arr);
+arr.shift();
+console.log(arr);
+console.log(arr.includes(5));
+console.log(arr.indexOf(4));
+console.log("A", arr);
+console.log(arr.slice(1,3));
+console.log("B",arr);
+console.log(arr.splice(1,3));
+
+console.log("C",arr);
