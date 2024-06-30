bcc258e2ad1be82e3fa558efc53ec84f4b390f88
Surend123 <sky958447@gmail.com>
2024-06-30 20:05:48 +0530

    Delete Date.js

--- Date.js
+++
@@ -1,8 +0,0 @@
-let myDate=new Date();
-console.log(typeof myDate);
-console.log(myDate.toString());
-let takeDate=new Date(2023,5,8,);
-console.log(Date.now());
-console.log(Date.parse(takeDate));
-myDate.getFullYear();
-console.log(myDate);
